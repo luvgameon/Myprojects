e922b1ac4d824d804ea969bd5672664f86aa9237
luvgameon <luvgameon@gmail.com>
2022-12-31 02:02:19 +0530

    test

---
+++ Expense Tracker React/src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        {" "}
+        <h1> Expense Item </h1>{" "}
+      </div>
+      <div>
+        {" "}
+        <h2>Food Rs 10</h2>
+      </div>
+      <div>
+        {" "}
+        <h2>Petrol Rs 100</h2>
+      </div>
+      <div>
+        {" "}
+        <h2>Movie Rs 110</h2>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
