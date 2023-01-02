1e357434be51531a68a4b8553fb2440a2fc226ba
luvgameon <luvgameon@gmail.com>
2023-01-02 15:18:56 +0530

    Custom date Component

--- Expense Tracker React/src/components/ExpenseItem.js
+++ Expense Tracker React/src/components/ExpenseItem.js
@@ -1,24 +1,14 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css'
 function ExpenseItem(props) {
+ 
 
   return (
     <div className='expense-item '>
-      <div >
-        {" "}
-        <h1 > {props.title}  </h1>{" "}
-      </div>
-      <div>
-        {" "}
-        <h2 className='expense-item__description'>{props.date.toISOString()}</h2>
-      </div>
-      <div>
-        {" "}
-        <h2 className='expense-item__description'>{props.location}</h2>
-      </div>
-      <div>
-        {" "}
-        <h2 className='expense-item__price'>{props.amount}</h2>
-      </div>
+     
+     <ExpenseDate date={props.date}/>
+     <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>
     </div>
   );
 }
