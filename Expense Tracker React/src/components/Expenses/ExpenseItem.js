c4ba3a6afffe8469938659d19d4370050a9e7dc6
luvgameon <luvgameon@gmail.com>
2023-01-20 01:42:12 +0530

    NewExpense

--- Expense Tracker React/src/components/Expenses/ExpenseItem.js
+++ Expense Tracker React/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,35 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import './ExpenseItem.css'
-function ExpenseItem(props) {
- 
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  const [amt, setAmt] = useState(props.amount);
+  
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+  const amountHandler = () => {
+    setAmt('100$');
+    
+  };
 
   return (
-    <div className='expense-item '>
-     
-     <ExpenseDate date={props.date}/>
-     <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>
-    </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amt}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={amountHandler}>Change Amount</button>
+    </Card>
   );
 }
+
 export default ExpenseItem;
