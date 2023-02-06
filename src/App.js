6b54ebf03d16a1b67d3e893fac1d900cfbee1fd4
luvgameon <luvgameon@gmail.com>
2023-02-06 23:12:24 +0530

    Basic Done

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import AddUser from './Components/Users/AddUser';
+import Showuser from './Components/Users/ShowUser';
+
+
+function App() {
+  const [userList, setuserList] = useState([]);
+  const addUserHandler=(uName,uAge)=>{
+    setuserList((prevdetails)=>{
+      return [
+        ...prevdetails,
+        {name:uName,age:uAge,id:Math.random().toString()}
+      ];
+    });
+
+  };
+  const cnt=userList.length;
+  if(cnt===0)
+  {
+    return (
+      <div>
+      <AddUser onAdduser={addUserHandler}/>
+      </div>
+
+    )
+  }
+  else{
+  return (
+    
+    
+    <div>
+    <AddUser onAdduser={addUserHandler}/>
+      <Showuser users={userList}/>
+
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,68 @@
+import React,{useState} from 'react';
+
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+
+import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
+
+
+const AddUser = (props) => {
+  const [username, setusername] = useState('');
+  const [age, setage] = useState('');
+  const [error,seterror]=useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if(username.trim().length===0 || age.trim().length===0){
+      seterror({
+        title:"Enter A Valid Input",
+        msg:'Please Enter correct age and Name'
+      })
+      return;
+    }
+    if(+age < 1){
+      seterror({
+        title:"Enter A Valid Age",
+        msg:'Age must be postive'
+      })
+      return; 
+    }
+    props.onAdduser(username,age);
+    setusername('');
+    setage('');
+    
+   
+   
+  
+  };
+  const changeusername=(event)=>{
+    setusername(event.target.value);
+
+  }
+  const changeage=(event)=>{
+    setage(event.target.value);
+
+  }
+  const errorhandler=()=>{
+    seterror(null);
+  }
+  
+
+  return (
+    <div>
+    { error && <ErrorModal title={error.title} msg={error.msg} onConfirm={errorhandler}></ErrorModal>}
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" value={username} onChange={changeusername}/>
+        <label htmlFor="age" >Age Years</label>
+        <input id="age" type="number" value={age} onChange={changeage}/>
+        <Button type='submit' > Add User</Button>
+      </form>
+    </Card>
+    </div>
+  );
+};
+
+export default AddUser;
